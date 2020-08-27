5b953c379ed5cabb53b07d5e0d4f3467f242f06f
odin-mebesius <odin@ttt.studio>
2020-08-27 13:50:37 -0700

    add uri image sources to example app

--- react-native-carousel-example-app/example/App.tsx
+++ react-native-carousel-example-app/example/App.tsx
@@ -18,6 +18,11 @@ const App = () => {
   const bulbasaur = require('./assets/bulbasaur.png')
   const squirtle = require('./assets/squirtle.png')
   const charmander = require('./assets/charmander.png')
+  const scyther = { uri: "https://cdn.bulbagarden.net/upload/b/ba/123Scyther.png" }
+  const lapras = { uri: "https://cdn.bulbagarden.net/upload/thumb/a/ab/131Lapras.png/1200px-131Lapras.png" }
+  const eevee = { uri: "https://cdn.bulbagarden.net/upload/thumb/e/e2/133Eevee.png/1200px-133Eevee.png" }
+  const snorlax = { uri: "https://cdn.bulbagarden.net/upload/thumb/f/fb/143Snorlax.png/1200px-143Snorlax.png" }
+  const dragonite = { uri: "https://cdn.bulbagarden.net/upload/8/8b/149Dragonite.png" }
   const mewtwo = require('./assets/mewtwo.png')
   const mew = require('./assets/mew.png')
 
@@ -25,6 +30,11 @@ const App = () => {
     bulbasaur,
     squirtle,
     charmander,
+    scyther,
+    lapras,
+    eevee,
+    snorlax,
+    dragonite,
     mewtwo,
     mew
   ]
@@ -32,7 +42,7 @@ const App = () => {
   return (
     <>
       <StatusBar barStyle='dark-content' />
-      <View style={{ flex: 1 }}>
+      <View style={{ flex: 1, paddingHorizontal: 20 }}>
         <RNCarousel sources={SOURCES}/>
       </View>
     </>
