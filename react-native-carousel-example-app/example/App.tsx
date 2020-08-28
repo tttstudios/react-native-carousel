9f13d6b198c371e2267d2f44ec95d07f1e39ff70
odin-mebesius <odin@ttt.studio>
2020-08-28 10:05:19 -0700

    add gifs

--- react-native-carousel-example-app/example/App.tsx
+++ react-native-carousel-example-app/example/App.tsx
@@ -18,11 +18,14 @@ const App = () => {
   const bulbasaur = require('./assets/bulbasaur.png')
   const squirtle = require('./assets/squirtle.png')
   const charmander = require('./assets/charmander.png')
-  const scyther = { uri: "https://cdn.bulbagarden.net/upload/b/ba/123Scyther.png" }
-  const lapras = { uri: "https://cdn.bulbagarden.net/upload/thumb/a/ab/131Lapras.png/1200px-131Lapras.png" }
-  const eevee = { uri: "https://cdn.bulbagarden.net/upload/thumb/e/e2/133Eevee.png/1200px-133Eevee.png" }
-  const snorlax = { uri: "https://cdn.bulbagarden.net/upload/thumb/f/fb/143Snorlax.png/1200px-143Snorlax.png" }
-  const dragonite = { uri: "https://cdn.bulbagarden.net/upload/8/8b/149Dragonite.png" }
+  const clefairy = { uri: 'https://media4.giphy.com/media/rAm0u2k17rM3e/giphy.gif' }
+  const haunter = require('./assets/haunter.gif')
+  const scyther = { uri: 'https://cdn.bulbagarden.net/upload/b/ba/123Scyther.png' }
+  const ditto = require('./assets/ditto.gif')
+  const lapras = { uri: 'https://cdn.bulbagarden.net/upload/thumb/a/ab/131Lapras.png/1200px-131Lapras.png' }
+  const eevee = { uri: 'https://cdn.bulbagarden.net/upload/thumb/e/e2/133Eevee.png/1200px-133Eevee.png' }
+  const snorlax = { uri: 'https://cdn.bulbagarden.net/upload/thumb/f/fb/143Snorlax.png/1200px-143Snorlax.png' }
+  const dragonite = { uri: 'https://cdn.bulbagarden.net/upload/8/8b/149Dragonite.png' }
   const mewtwo = require('./assets/mewtwo.png')
   const mew = require('./assets/mew.png')
 
@@ -30,7 +33,10 @@ const App = () => {
     bulbasaur,
     squirtle,
     charmander,
+    clefairy,
+    haunter,
     scyther,
+    ditto,
     lapras,
     eevee,
     snorlax,
